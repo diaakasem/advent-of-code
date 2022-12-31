8fc4837f6e0e06c7e939f4a75caf78d7b7f8d9ce
Diaa Kasem <me@diaa.me>
2022-12-31 23:45:24 +0200

    Correctly organizing Day 1 & Day 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -31,11 +31,20 @@ fn init_elves(lines: &Vec<String>) -> ElvesPact {
 fn main() {
     let args = Args::parse();
     let lines = read_file_lines(&args.data);
-    let elves = init_elves(&lines);
+    let mut elves = init_elves(&lines);
     elves.display_elves();
     println!("=======================================");
+    part_1(&mut elves);
+    println!("=======================================");
+    part_2(&mut elves);
+}
+
+fn part_1(elves: &mut ElvesPact) {
     let elve = elves.get_elve_with_most_calories();
     println!("The Elve with the most calories = {} calories", elve.total_calories());
+}
 
-
+fn part_2(elves: &mut ElvesPact) {
+    let elves = elves.get_n_elves_with_most_calories(3);
+    println!("Elves with the most calories = {} calories", elves.get_total_calories());
 }
--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
-use elves::ElvesPact;
+use elves::Elve;
 use ioutils::read_file_lines;
+use rock_paper_scissors::{Game, Hand, Player};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -10,30 +11,96 @@ struct Args {
     data: String,
 }
 
-fn init_elves(lines: &Vec<String>) -> ElvesPact {
-    let mut elves = ElvesPact::new();
-    let mut i = 0;
+fn mapper(line: &str) -> Hand {
+    match line {
+        "A" => Hand::Rock,
+        "X" => Hand::Rock,
+        "B" => Hand::Paper,
+        "Y" => Hand::Paper,
+        "C" => Hand::Scissors,
+        "Z" => Hand::Scissors,
+        _ => panic!("Unknown Rock Paper Scissors Hand"),
+    }
+}
+
+fn pick_hand(opponent_hand: &Hand, lose_win_tie: &str) -> Hand {
+    match lose_win_tie {
+        "Z" => match opponent_hand {
+            Hand::Rock => Hand::Paper,
+            Hand::Paper => Hand::Scissors,
+            Hand::Scissors => Hand::Rock,
+        },
+        "X" => match opponent_hand {
+            Hand::Rock => Hand::Scissors,
+            Hand::Paper => Hand::Rock,
+            Hand::Scissors => Hand::Paper,
+        },
+        "Y" => match opponent_hand {
+            Hand::Rock => Hand::Rock,
+            Hand::Paper => Hand::Paper,
+            Hand::Scissors => Hand::Scissors,
+        },
+        _ => panic!("Unknown Rock Paper Scissors Hand"),
+    }
+}
+
+fn init_explained_game(lines: &Vec<String>) -> Game {
+    let player1 = Player::new(Elve::new(1 as u8));
+    let player2 = Player::new(Elve::new(2 as u8));
+    let mut game = Game::new(player1, player2);
     lines.iter().for_each(|line| {
-        if line.len() > 0 {
-            if i == 0 {
-                elves.new_elve();
-            }
-            let calories = line.parse::<u32>().expect("Not a number");
-            elves.add_meal_by_calories_to_last_elve(calories);
-        } else {
-            elves.new_elve();
+        if line.len() == 0 {
+            return;
         }
-        i += 1;
+        let hands = line
+            .split_whitespace()
+            .collect::<Vec<&str>>();
+        let opponent_hand = mapper(hands[0]);
+        let lose_win_tie = hands[1];
+        game.play(opponent_hand, pick_hand(&opponent_hand, lose_win_tie));
     });
-    elves
+    game
+}
+
+
+fn init_game(lines: &Vec<String>) -> Game {
+    let player1 = Player::new(Elve::new(1 as u8));
+    let player2 = Player::new(Elve::new(2 as u8));
+    let mut game = Game::new(player1, player2);
+    lines.iter().for_each(|line| {
+        if line.len() == 0 {
+            return;
+        }
+        let hands = line
+            .split_whitespace()
+            .map(|s| mapper(s))
+            .collect::<Vec<Hand>>();
+        game.play(hands[0], hands[1]);
+    });
+    game
 }
 
 fn main() {
     let args = Args::parse();
     let lines = read_file_lines(&args.data);
-    let elves = init_elves(&lines);
-    elves.display_elves();
+    part_1(&lines);
+    part_2(&lines);
+}
+
+fn part_1(lines: &Vec<String>) {
+    let game = init_game(&lines);
     println!("=======================================");
-    let elves = elves.get_n_elves_with_most_calories(3);
-    println!("Elves with the most calories = {} calories", elves.get_total_calories());
+    println!(
+        "My expected score with the elve cheatsheet is = {}",
+        game.get_player2().get_score()
+    );
+}
+
+fn part_2(lines: &Vec<String>) {
+    let game = init_explained_game(&lines);
+    println!("=======================================");
+    println!(
+        "My expected score with the elve cheatsheet after explaination is = {}",
+        game.get_player2().get_score()
+    );
 }
